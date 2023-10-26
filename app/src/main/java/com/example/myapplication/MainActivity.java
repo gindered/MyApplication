52ee00b7110c422b54ca735056c052c674ab4169
dermann2811@gmail.com <dermann2811@gmail.com>
2023-10-26 13:50:05 +0200

    added display for done items and added styling for lists

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.commons.io.FileUtils;
 
+// ToDo
+// - Toolbar
+// - Delet Button durch swipen ersetzten
+// x Erledigte durchstreichen
+// x Listen an Toolbar anhängen
+// x Leerer Text soll nicht hinzugefügt werden können
+// - speichern der erledigten Items
+
 //Die `MainActivity.java` ist eine Java-Datei in Android Studio, die eine Aktivitätsklasse
 // repräsentiert. Eine Aktivität ist ein grundlegender Bestandteil einer Android-App und
 // repräsentiert normalerweise ein Fenster, das dem Benutzer angezeigt wird
@@ -22,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
     //Android bereitgestellte Basisaktivitätsklasse ist
 
     private ArrayList<String> items;
+    private ArrayList<String> itemsDone;
     private ArrayAdapter<String> itemsAdapter;
+    private ArrayAdapter<String> itemsDoneAdapter;
     private ListView lvItems;
+    private ListView lvItemsDone;
 
     @Override
     // "@Override" ein Hinweis für den Compiler, dass die folgende Methode
@@ -47,12 +58,18 @@ public class MainActivity extends AppCompatActivity {
 
         //list = findViewById(R.id.list);
         lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItemsDone = (ListView) findViewById(R.id.lvItemsDone);
         items = new ArrayList<>();
+        itemsDone = new ArrayList<>();
+
         readItems();
         itemsAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, items);
-
+                android.R.layout.simple_list_item_multiple_choice, items);
         lvItems.setAdapter(itemsAdapter);
+
+        itemsDoneAdapter = new ArrayAdapter<>(this,
+                R.layout.layout_list_done, itemsDone);
+        lvItemsDone.setAdapter(itemsDoneAdapter);
 
         setupListViewListener();
     }
@@ -81,10 +98,13 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setOnItemLongClickListener(
                 (adapter, item, pos, id) -> {
                     // Remove the item within array at position
+                    itemsDoneAdapter.add(items.get(pos));
                     items.remove(pos);
+                    //itemsDone.add(items.get(pos));
 
                     // Refresh the adapter
                     itemsAdapter.notifyDataSetChanged();
+                    itemsDoneAdapter.notifyDataSetChanged();
                     // Return true consumes the long click event (marks it handled)
                     writeItems();
                     return true;
@@ -92,10 +112,40 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setOnItemClickListener(
                 (parent, view, pos, id) -> {
                     // Remove the item within array at position
+                    itemsDoneAdapter.add(items.get(pos));
                     items.remove(pos);
+                    //itemsDone.add(items.get(pos));
 
                     // Refresh the adapter
                     itemsAdapter.notifyDataSetChanged();
+                    itemsDoneAdapter.notifyDataSetChanged();
+                    // Return true consumes the long click event (marks it handled)
+                    writeItems();
+                });
+        lvItemsDone.setOnItemLongClickListener(
+                (adapter, item, pos, id) -> {
+                    // Remove the item within array at position
+                    itemsAdapter.add(itemsDone.get(pos));
+                    itemsDone.remove(pos);
+                    //itemsDone.add(items.get(pos));
+
+                    // Refresh the adapter
+                    itemsAdapter.notifyDataSetChanged();
+                    itemsDoneAdapter.notifyDataSetChanged();
+                    // Return true consumes the long click event (marks it handled)
+                    writeItems();
+                    return true;
+                });
+        lvItemsDone.setOnItemClickListener(
+                (parent, view, pos, id) -> {
+                    // Remove the item within array at position
+                    itemsAdapter.add(itemsDone.get(pos));
+                    itemsDone.remove(pos);
+                    //itemsDone.add(items.get(pos));
+
+                    // Refresh the adapter
+                    itemsAdapter.notifyDataSetChanged();
+                    itemsDoneAdapter.notifyDataSetChanged();
                     // Return true consumes the long click event (marks it handled)
                     writeItems();
                 });
@@ -104,7 +154,9 @@ public class MainActivity extends AppCompatActivity {
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
+        if (!itemText.equals("")) {
+            itemsAdapter.add(itemText);
+        }
         etNewItem.setText("");
         writeItems();
     }
